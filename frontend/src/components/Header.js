1299d0fbfdb71aec6362e3249bdf9e01f401c6e8
AlvinLogen <alvin.logenstein@gmail.com>
2023-03-12 00:30:09 +0200

    Updated Navigation Bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 
 const Header = ({ title }) => {
+  const logo = "android-chrome-512x512.png";
   return (
-    <Navbar bg="light" variant="light">
+    <Navbar bg="dark" variant="dark">
+      <a class="navbar-brand" href="#">
+        <img src={logo} width="40" height="40"></img>
+      </a>
       <Navbar.Brand href="/">{title}</Navbar.Brand>
     </Navbar>
   );
