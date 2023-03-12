e6c0028c3dd82a67a279d45bd9f411d6c58100a7
AlvinLogen <alvin.logenstein@gmail.com>
2023-03-12 14:27:42 +0200

    First API Call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_API = "https://api.unsplash.com";
+const UNSPLASH_PATH = "/photos/random/";
+
 const App = () => {
   const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+
+    fetch(
+      `${UNSPLASH_API}${UNSPLASH_PATH}?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
