8d40e9ddf1e72fa0744ebc9c1e91de57bc4eac91
AlvinLogen <alvin.logenstein@gmail.com>
2023-03-13 20:45:05 +0200

    Display and Delete Images

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const ImageCard = () => {
+const ImageCard = ({ image, deleteImage }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img
-        variant="top"
-        src="https://images.unsplash.com/photo-1481567681190-7584beb89c1f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MjEzNzV8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Nzg3MTcyMzk&ixlib=rb-4.0.3&q=80&w=400"
-      />
+      <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
       </Card.Body>
     </Card>
   );
