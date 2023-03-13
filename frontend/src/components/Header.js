c8a3cebf1d27d288674f2ab20d458fb9809ba3f4
AlvinLogen <alvin.logenstein@gmail.com>
2023-03-13 14:18:39 +0200

    Removed Lint Prettier Config

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
 const navbarStyle = {
   backgroundColor: "black",
 };
 
 const logo = "android-chrome-512x512.png";
+const size = "40";
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="dark">
       <a>
-        <img src={logo} width="40" height="40"></img>
+        <img src={logo} width={size} height={size}></img>
       </a>
       <Navbar.Brand className="mx-3 text-uppercase" href="/">
         {title}
